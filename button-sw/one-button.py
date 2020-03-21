c9065ae58df1340f88710988c805bf061ed63d94
Timur Sattarov <tim.helloworld@gmail.com>
2020-03-22 01:08:15 +0400

    Initial commit

--- button-sw/one-button.py
+++ button-sw/one-button.py
@@ -2,13 +2,21 @@ import paho.mqtt.client as paho
 import argparse
 import logging
 import time
+import json
+
+configuration = {}
+"""
+  the variable is a dictionary with "latitude", "longitude" and "url" keys
+"""
 
 logging.basicConfig(level=logging.DEBUG)
 
 LOGGER = logging.getLogger(__name__)
 HOST = "sandbox.rightech.io"
 PORT = 1883
-CLIENT_ID = "mqtt-hu2020_03-fdsvh4"
+CLIENT_ID = "mqtt-hu2020_03-mlq1gs"
+ONE_BUTT_CONFIGURE_SET = "one-butt/configure/set"
+ONE_BUTT_DISPLAY_SET = "one-butt/display/set"
 
 
 def on_connect(client, userdata, flags, rc):
@@ -16,14 +24,18 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    LOGGER.debug(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    LOGGER.info("%s (%s): %s %s" % (msg.topic, client, str(msg.qos), str(msg.payload)))
+    if msg.topic == ONE_BUTT_CONFIGURE_SET:
+        global configuration
+        configuration = json.loads(msg.payload)
 
 
 client = paho.Client(CLIENT_ID)
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(HOST, PORT)
-client.subscribe("one-butt/#")
+client.subscribe(ONE_BUTT_CONFIGURE_SET)
+client.subscribe(ONE_BUTT_DISPLAY_SET)
 client.loop_start()
 
 LOGGER.info("loop started")
@@ -32,9 +44,10 @@ try:
     while True:
         temperature += 0.1
         (rc, mid) = client.publish("one-butt/event/new-pin", str(temperature), qos=1)
-        (rc, mid) = client.publish("lat", 56)
-        (rc, mid) = client.publish("long", 53)
-        LOGGER.debug("pin generated: %s" % (str(temperature)))
+        if configuration:
+            (rc, mid) = client.publish("pos.lat", configuration["latitude"])
+            (rc, mid) = client.publish("pos.lon", configuration["longitude"])
+        LOGGER.info("new pin generated: %s" % (str(temperature)))
         time.sleep(5)
 finally:
     client.loop_stop()
