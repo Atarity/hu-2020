840a13b4196a7e42d7fd1b48050367e19331f0a8
Timur Sattarov <tim.helloworld@gmail.com>
2020-03-22 16:59:05 +0400

    background status pub implemented

--- button-sw/one-button.py
+++ button-sw/one-button.py
@@ -5,7 +5,10 @@ import argparse
 import logging
 import time
 import json
+import threading
+import functools
 
+config_lock = threading.RLock()
 configuration = {}
 """
   the variable is a dictionary with "latitude", "longitude" and "url" keys
@@ -19,6 +22,8 @@ PORT = 1883
 ONE_BUTT_CONFIGURE_SET = "one-butt/configure/set"
 ONE_BUTT_DISPLAY_SET = "one-butt/display/set"
 
+running = True
+
 
 def on_connect(client, userdata, flags, rc):
     LOGGER.debug("CONNACK received with code %d." % (rc))
@@ -28,7 +33,20 @@ def on_message(client, userdata, msg):
     LOGGER.info("%s (%s): %s %s" % (msg.topic, client, str(msg.qos), str(msg.payload)))
     if msg.topic == ONE_BUTT_CONFIGURE_SET:
         global configuration
-        configuration = json.loads(msg.payload)
+        global config_lock
+        with config_lock:
+            configuration = json.loads(msg.payload)
+            pub_configuration(client)
+
+
+def pub_configuration(client):
+    global configuration
+    global config_lock
+    if configuration:
+        with config_lock:
+            (rc, mid) = client.publish("pos.lat", configuration["latitude"])
+            (rc, mid) = client.publish("pos.lon", configuration["longitude"])
+            (rc, mid) = client.publish("one-butt/state/url-template", configuration["url"])
 
 
 def get_battery_level():
@@ -36,8 +54,15 @@ def get_battery_level():
 
 
 def generate_pin():
-
     return "asomepin" + str(int(random.random()*10000))
+
+
+def heartbeat_worker(client):
+    global running
+    while running:
+        (rc, mid) = client.publish("one-butt/state/bat-charge", get_battery_level())
+        LOGGER.debug("one-butt/state/bat-charge: %s, %s" % (str(rc), str(mid)))
+        time.sleep(5)
 
 
 if __name__ == "__main__":
@@ -53,7 +78,8 @@ if __name__ == "__main__":
     client.subscribe(ONE_BUTT_CONFIGURE_SET)
     client.subscribe(ONE_BUTT_DISPLAY_SET)
     client.loop_start()
-
+    thread = threading.Thread(target=functools.partial(heartbeat_worker, client))
+    thread.start()
     LOGGER.info("loop started")
     temperature = 36.6
     try:
@@ -61,14 +87,11 @@ if __name__ == "__main__":
             pin = generate_pin()
             (rc, mid) = client.publish("one-butt/event/new-pin", pin, qos=1)
             LOGGER.debug("one-butt/event/new-pin: %s, %s" % (str(rc), str(mid)))
-            (rc, mid) = client.publish("one-butt/state/bat-charge", get_battery_level())
-            LOGGER.debug("one-butt/state/bat-charge: %s, %s" % (str(rc), str(mid)))
-            if configuration:
-                (rc, mid) = client.publish("pos.lat", configuration["latitude"])
-                (rc, mid) = client.publish("pos.lon", configuration["longitude"])
             LOGGER.info("new pin generated: %s" % pin)
-            time.sleep(5)
+            time.sleep(13)
     finally:
+        running = False
+        thread.join()
         client.loop_stop()
         LOGGER.info("loop stopped")
 
